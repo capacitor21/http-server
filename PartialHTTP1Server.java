0096b5aeaf446dd61bdff68fca3363a8de93d58b
Michael DeDreu <mdedreu21@gmail.com>
2020-09-16 09:54:38 -0400

    Added skeleton of main file and socket handler thread

---
+++ PartialHTTP1Server.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.net.*;
+
+public PartialHTTP1Server {
+
+    
+
+    public static void main(String[] args) {
+        int port = Integer.parseInt(args[0]);
+        
+    }
+}
---
+++ SocketHandler.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.net.*;
+
+public class SocketHandler extends Thread {
+    public SocketHandler(Socket s) {
+
+    }
+    public void run() {
+
+    }
+}
