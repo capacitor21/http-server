68e70f7d0e226bc87f453e597b9d72d7e4471c26
Michael DeDreu <mdedreu21@gmail.com>
2020-09-16 22:13:33 -0400

    Rough layout of code structure

--- PartialHTTP1Server.java
+++ PartialHTTP1Server.java
@@ -1,12 +1,36 @@
 import java.io.*;
 import java.net.*;
 
-public PartialHTTP1Server {
+public class PartialHTTP1Server {
 
+    public static ServerSocket serv; //ServerSocket that listens for connections
+    public static int port; 
+    public static int numThreads; //How many threads are currently created
+
+    public static Set<SocketHandler> threads; //Possible data stucture to store current threads???
     
 
     public static void main(String[] args) {
-        int port = Integer.parseInt(args[0]);
+        port = Integer.parseInt(args[0]); //Takes port number from input
+        try {
+            serv = new ServerSocket(port); 
+        } catch(IOException e) {
+            e.printStackTrace();
+            //Do something
+        }
+
+        while(true) {
+            try {
+                Socket t = serv.accept();
+            
+                SocketHandler handler = new SocketHandler(t);
+                handler.start();
+
+            } catch(IOException e) {
+                e.printStackTrace();
+            }
+        }
         
+
     }
 }
--- SocketHandler.java
+++ SocketHandler.java
@@ -2,10 +2,19 @@ import java.io.*;
 import java.net.*;
 
 public class SocketHandler extends Thread {
-    public SocketHandler(Socket s) {
 
+    private Socket s; //Socket passed to the current thread
+    private BufferedReader in;
+
+    public SocketHandler(Socket s) {
+        this.s = s;
     }
     public void run() {
-
-    }
+        try {
+            in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+            System.out.print(in.readLine());
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+     }
 }
