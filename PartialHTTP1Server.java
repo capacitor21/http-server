14009dd334d0b6735e26a0775664f922b6b33df3
Christopher DeAngelis <45888417+numbers24@users.noreply.github.com>
2020-09-23 20:14:20 -0400

    Update PartialHTTP1Server.java

--- PartialHTTP1Server.java
+++ PartialHTTP1Server.java
@@ -1,45 +1,45 @@
 import java.io.*;
 import java.net.*;
 import java.util.*;
+//imports for thread pool:
 import java.util.concurrent.ExecutorService; 
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.RejectedExecutionException;
+import java.util.concurrent.TimeUnit;
+
 
 public class PartialHTTP1Server {
 
     public static ServerSocket serv; //ServerSocket that listens for connections
     public static int port;
-
-    public static ArrayList<SocketHandler> threads; //Possible data stucture to store current threads???
     
     public static void main(String[] args) {
         port = Integer.parseInt(args[0]); //Takes port number from input
-        threads = new ArrayList<SocketHandler>();
+
         try {
             serv = new ServerSocket(port); 
         } catch(IOException e) {
             e.printStackTrace();
             //Do something
         }
-        ThreadPoolExecutor tpool = (ThreadPoolExecutor) Executors.newFixedThreadPool(5); //thread pool object
+        ExecutorService tpool = new ThreadPoolExecutor(5,50,1000,TimeUnit.MILLISECONDS, new LinkedBlockingQueue<>()); //thread pool object
         while(true) {
             try {
                 Socket s = serv.accept();
-                if(tpool.getPoolSize()==50){
-                
-                    try{
+                try{
+                    SocketHandler handler = new SocketHandler(s);
+                    tpool.execute(handler);
+                } catch (RejectedExecutionException e){
+                     try{
                         BufferedWriter resp = new BufferedWriter(new OutputStreamWriter(s.getOutputStream()));
                         resp.write(Response.getErrorMessage(503));
                         resp.flush();
                         resp.close();
-                    } catch (IOException e) {
-                        e.printStackTrace();
+                    } catch (IOException e2) {
+                        e2.printStackTrace();
                     }
-                }
-                else{
-                    SocketHandler handler = new SocketHandler(s);
-                    threads.add(handler);
-                    tpool.execute(handler);
                 }
             } catch(IOException e) {
                 e.printStackTrace();
